7b9e223a9f1ce1185eb00f56727d52a4a8df9720
Shravan55555 <134358009+Shravan55555@users.noreply.github.com>
2024-10-18 08:49:25 +0530

    Create vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,18 @@
+HW=hardware/lineage
+POWER=hardware/lineage/interfaces/power-libperfmgr/aidl
+if [ -f "$POWER/Power.cpp" ]; then
+    echo -e "Fixing DT2W"
+    sleep 2
+    echo "Power.cpp found! Removing now..."
+    if rm -rf $POWER/Power.cpp; then
+        echo "Removing Power.cpp success! Copying modified Power.cpp now..."
+        if cp -rf $HW/power/Power.cpp $POWER; then
+            echo "Copying succesfull"
+            rm -rf $HW/vendorsetup.sh
+        else
+            echo "Copying failed"
+        fi
+    else
+        echo "Removing Power.cpp failed!"
+    fi
+fi
